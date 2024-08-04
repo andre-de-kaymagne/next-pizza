19087c8c8a6829ec74910d7d686cde026f005a99
andre-de-kaymagne <akaymanov@yandex.ru>
2024-08-04 13:58:57 +0300

    Выровнял хидер

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import RootLayout from "./layout";
+
 
 export default function Home() {
   return (
-    <h1>ЛЮБИМАЯ, НЕ ГОРЮЙ
-      <Button variant={"outline"}>Корзина</Button>
-    </h1>
+    <h1></h1>
   );
 }
--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -13,8 +13,12 @@ export const Header: React.FC<Props> = ({ className }) => {
       <Container className='flex items-center justify-between py-8'>
 
         {/* Левая часть */}
-        <div>
-          <Image src='/logo.svg' alt='logo' width={32} height={32} />
+        <div className='flex items-center gap-4'>
+          <Image src='/logo.svg' alt='logo' width={35} height={35} />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+          </div>
         </div>
 
       </Container>
