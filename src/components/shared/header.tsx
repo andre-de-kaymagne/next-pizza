c385c0ff4bdf9f7939f80322bca70d30a4b7411e
andre-de-kaymagne <akaymanov@yandex.ru>
2024-08-04 14:40:40 +0300

    Добавил логотип

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -14,7 +14,12 @@ export const Header: React.FC<Props> = ({ className }) => {
 
         {/* Левая часть */}
         <div className='flex items-center gap-4'>
-          <Image src='/logo.svg' alt='logo' width={35} height={35} />
+          <Image 
+          src='/logo.png' 
+          alt='logo' 
+          width={35} 
+          height={35} 
+          />
           <div>
             <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
             <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
