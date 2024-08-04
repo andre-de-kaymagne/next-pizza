19107fdce584fd1d81cdb780e0b2ec64856b90cb
andre-de-kaymagne <akaymanov@yandex.ru>
2024-08-04 18:13:08 +0300

    добавил пейдж и сорт

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import RootLayout from "./layout";
+import { Container, Title, Categories } from "@/components/shared";
+
 
 
 export default function Home() {
-  return (
-    <h1></h1>
-  );
+  return <>
+
+  <Container className="mt-10">
+    <Title text="Все пиццы" size="lg" className="font-extrabold" />
+    <Categories />
+  </Container>
+  
+  </>
 }
