de93d6ba01237ca0e7279fc63878f3fcf7e1d536
andre-de-kaymagne <akaymanov@yandex.ru>
2024-08-08 16:35:39 +0300

    Создали фильтрацию

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import RootLayout from "./layout";
-import { Container, Title, Categories } from "@/components/shared";
+import { Container, Title, TopBar, Categories, SortPopup, Filters } from "@/components/shared";
+
 
 
 
@@ -10,7 +11,25 @@ export default function Home() {
 
   <Container className="mt-10">
     <Title text="Все пиццы" size="lg" className="font-extrabold" />
-    <Categories />
+  
+  </Container>
+
+  <TopBar />
+
+  <Container className="mt-10 pb-14">
+    <div className="flex gap-[60px]">
+
+      {/* Фильрация */}
+      <div className="w-[250px]">
+        <Filters />
+      </div>
+
+
+      {/* Список товаров */}
+      <div className="flex-1">
+        <div className="flex flex-col gap-16">Список товаров</div>
+        </div>   
+      </div>
   </Container>
   
   </>
